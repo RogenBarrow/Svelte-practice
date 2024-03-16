04369660dc4fc34fa35a43b51f94e41965f11154
Devratb <93407364+Devratb@users.noreply.github.com>
2024-03-16 19:06:38 -0400

    pdf functionality

--- src/routes/displaydata/+page.server.js
+++ src/routes/displaydata/+page.server.js
@@ -1,12 +1,11 @@
+//import { pdfPrint } from '$lib/components/pdf';
 import getSupaData from '$lib/supabaseData';
-import { data } from 'autoprefixer';
+//import { data } from 'autoprefixer';
 
 export const load = async () => {
     const data = await getSupaData();
-    
-    //execute store data in file
 
-    // console.log('The data in the load function: ', data);
+    //execute store data in file
 
     return {
         supaData: data,

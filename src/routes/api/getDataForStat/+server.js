746af4759cd88e9ba9976e7526ebef668fcb8d65
Devratb <93407364+Devratb@users.noreply.github.com>
2024-12-04 22:12:23 -0400

    database name change

--- src/routes/api/getDataForStat/+server.js
+++ src/routes/api/getDataForStat/+server.js
@@ -1,6 +1,5 @@
 import getSupaSum from '$lib/supabaseSum';
 
-/** @type {import('./$types').RequestHandler} */
 export async function GET() {
     const data = await getSupaSum();
     console.log('This is what the get will send: ', data);
